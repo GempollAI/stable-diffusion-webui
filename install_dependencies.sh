e58c6e9853908917b7a18652683113012feb357c
maple@max <feng.liang@kaust.edu.sa>
2023-11-23 15:57:27 +0800

    add dependency install script

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/zsh
+source /home/jenson/miniforge3/bin/activate
+
+mamba install -y GitPython einops fastapi gradio inflection jsonmerge kornia lark numpy omegaconf open-clip-torch piexif psutil requests safetensors scikit-image timm torchdiffeq
+python -m pip install -y accelerate basicsr blendmodes clean-fid gfpgan pytorch_lightning realesrgan resize-right tomesd torchsde
+mamba install -y -c huggingface transformers
