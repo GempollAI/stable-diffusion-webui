34644b02125e45c58796f7dec929fa5c896feaaa
maple@max <feng.liang@kaust.edu.sa>
2024-12-17 17:15:36 +0800

    fix

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 source /home/jenson/miniforge3/bin/activate
-python -m pip install -f requirements_versions.txt
+python -m pip install -r requirements_versions.txt
 python -m pip install tensorrt huggingface
 python -m pip install -U xformers --index-url https://download.pytorch.org/whl/cu124
 mamba clean --all -y
