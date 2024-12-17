068c1f341813db535d5820d6283d212ad7f2f1a7
maple@max <feng.liang@kaust.edu.sa>
2024-12-17 17:23:10 +0800

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 source /home/jenson/miniforge3/bin/activate
+unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY ALL_PROXY
+pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
 python -m pip install -r requirements_versions.txt
 python -m pip install tensorrt huggingface
 python -m pip install -U xformers --index-url https://download.pytorch.org/whl/cu124
