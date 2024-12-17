2dee1a8c7a4689d2804348b3ff019f877eff2b55
maple@max <feng.liang@kaust.edu.sa>
2024-12-17 17:11:29 +0800

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/zsh
 source /home/jenson/miniforge3/bin/activate
-unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY ALL_PROXY
-mamba install -y GitPython einops fastapi gradio inflection jsonmerge kornia lark numpy omegaconf open-clip-torch piexif psutil requests safetensors scikit-image timm torchdiffeq
-mamba install -y -c huggingface transformers
-mamba install -y xformers -c xformers
 python -m pip install -f requirements_versions.txt
-python -m pip install tensorrt
+python -m pip install tensorrt huggingface
+python -m pip install -U xformers --index-url https://download.pytorch.org/whl/cu124
 mamba clean --all -y
 python -m pip cache purge
